6fd97de7965766da91ba71b31b05487a74e87744
yangcanlaile <13710097312@163.com>
2019-01-08 11:20:45 +0800

    通用提交

---
+++ src/main/java/com/jiagouedu/pojo/User.java
@@ -0,0 +1,53 @@
+package com.jiagouedu.pojo;
+
+public class User {
+    private Integer id;
+
+    private String username;
+
+    private Integer age;
+
+    private String phone;
+
+    private String des;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public void setDes(String des) {
+        this.des = des == null ? null : des.trim();
+    }
+}
